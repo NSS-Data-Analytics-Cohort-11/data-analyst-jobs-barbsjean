bb171a29696d70b7079a871e7fe7002081e14129
barbsjean <barbarajbrake@gmail.com>
2024-02-10 10:38:42 -0600

    final push after class dicussion. I think my table may still be double or even tripled still?? I tried to fix it!

--- scripts/scripts for Indeed Assignment.sql
+++ scripts/scripts for Indeed Assignment.sql
@@ -1,9 +1,9 @@
 --1.	How many rows are in the data_analyst_jobs table?
 
-Select *
+Select COUNT(*)
 FROM data_analyst_jobs;
 
--- ANSWER: 3586
+-- ANSWER: 1793
 
 **************************************
 
@@ -38,9 +38,9 @@ WHERE location IN ('TN', 'KY');
 
 SELECT COUNT(*)
 FROM data_analyst_jobs
-WHERE star_rating > 4;
+WHERE LOCATION = 'TN' AND star_rating > 4;
 
---ANSWER: 832
+--ANSWER: 6
 
 **************************************
 
@@ -87,47 +87,47 @@ ANSWER: 230
 
 --9.	Find the name of each company and its average star rating for all companies that have more than 5000 reviews across all locations. How many companies are there with more that 5000 reviews across all locations?
 
-SELECT DISTINCT company, AVG(star_rating) AS avg_star_rating, review_count
-FROM data_analyst_jobs
+SELECT DISTINCT company, AVG(star_rating) AS avg_star_rating
+FROM indeed_data_analyst_jobs
 WHERE review_count > 5000
 AND company IS NOT NULL
-GROUP BY review_count, location, company;   --may have to rework this query to get the right answer... something to do with adding in the companies
+GROUP BY company;   -- don't have to add in distinct in SELECT when you are also group by that data
 
---ANSWER: 45
+--ANSWER: 40
 
 **************************************
 
 --10.	Add the code to order the query in #9 from highest to lowest average star rating. Which company with more than 5000 reviews across all locations in the dataset has the highest star rating? What is that rating?
 
-SELECT DISTINCT company, AVG(star_rating) AS avg_star_rating, review_count
-FROM data_analyst_jobs
+SELECT company, AVG(star_rating) AS avg_star_rating
+FROM indeed_data_analyst_jobs
 WHERE review_count > 5000
 AND company IS NOT NULL
-GROUP BY review_count, location, company
-ORDER BY avg_star_rating DESC; 
+GROUP BY company
+ORDER BY avg_star_rating DESC, company; 
 
---ANSWER: American Express, 4.1999 rating
+--ANSWER: American Express, 4.1999 rating -- 6 WAY TIE
 
 **************************************
 
 --11.	Find all the job titles that contain the word ‘Analyst’. How many different job titles are there? 
 
-SELECT title
+SELECT DISTINCT title
 FROM data_analyst_jobs
 WHERE title ILIKE '%Analyst%';
 
---ANSWER: 3338
+--ANSWER: 774
 
 **************************************
 
 --12.	How many different job titles do not contain either the word ‘Analyst’ or the word ‘Analytics’? What word do these positions have in common?
 
-SELECT *
-FROM data_analyst_jobs
+SELECT title
+FROM indeed_data_analyst_jobs
 WHERE title NOT ILIKE '%Analyst%'
 AND title NOT ILIKE '%Analytics%';
 
---ANSWER: 8. Data.
+--ANSWER: 4. Tableau.
 
 **************************************
 
@@ -137,7 +137,7 @@ AND title NOT ILIKE '%Analytics%';
 -- Order your results so that the domain with the greatest number of `hard to fill` jobs is at the top. 
 -- Which three industries are in the top 4 on this list? How many jobs have been listed for more than 3 weeks for each of the top 4?
 
-SELECT domain, COUNT(*)
+SELECT domain AS industry, COUNT(*)
 FROM data_analyst_jobs
 WHERE skill ILIKE '%SQL%'
 AND domain IS NOT NULL
@@ -145,4 +145,4 @@ AND days_since_posting > 21
 GROUP BY domain
 ORDER BY COUNT(*) DESC;
 
---ANSWER: Internet and Software (124), Banks and Financial Services (122), & Consulting and Business Services (114)
+--ANSWER: Internet and Software (186), Banks and Financial Services (183), & Consulting and Business Services (171)  -- think it may still be doubled??
